c4c4132f0372c5bdd63b5c0dfd1634f29251c6d6
Tanner Bowman <tannerbowman@gmail.com>
2025-06-18 20:39:26 -0600

    Key

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from clients.routes import clients_bp
 app = Flask(__name__)
 
 # It's good practice to set a secret key for sessions and WTForms
-app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'a_default_fallback_key_for_local_dev')
+
 
 # Register the blueprint
 app.register_blueprint(clients_bp)
